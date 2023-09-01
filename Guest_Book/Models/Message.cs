1b951d4d0c968ddd50b4f965d7a39d200efd6916
Sofia <sofiiagudz@gmail.com>
2023-09-01 20:24:28 +0300

    Task1. Доработки верстки

--- Guest_Book/Models/Message.cs
+++ Guest_Book/Models/Message.cs
@@ -5,7 +5,10 @@ namespace Guest_Book.Models
     public class Message
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string Message_ { get; set; }
+
         public DateTime MessageDate { get; set; }
         public string UserName { get; set; }
         public User User { get; set; }
